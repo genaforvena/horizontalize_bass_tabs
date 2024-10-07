6919618e5ddf39d372dc46da2b5f4bda2a0b5d8d
Ilya M <ilyailiilia@gmail.com>
2024-10-07 17:59:59 +0200

    Pass line length as argument

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 import re
+import sys
 
 def download_tab(url):
     try:
@@ -16,7 +17,7 @@ def download_tab(url):
 def clear_line(line):
     return re.sub(r'[^0-9|-]', '', line)
 
-def extract_and_combine_tabs(text):
+def extract_and_combine_tabs(text, max_length=400):
     # Extract tab sections
     tab_pattern = r'\[tab\](.*?)\[/tab\]'
     tab_sections = re.findall(tab_pattern, text, re.DOTALL)
@@ -32,22 +33,22 @@ def extract_and_combine_tabs(text):
         for line in lines:
             clean_line = clear_line(line)
             if 'G|' in line:
-                if not g_strings or len(g_strings[-1]) + len(line) + 1 > 400:
+                if not g_strings or len(g_strings[-1]) + len(line) + 1 > max_length:
                     g_strings.append(clean_line)
                 else:
                     g_strings[-1] += (clean_line) + " "
             if 'D|' in line:
-                if not d_strings or len(d_strings[-1]) + len(line) + 1 > 400:
+                if not d_strings or len(d_strings[-1]) + len(line) + 1 > max_length:
                     d_strings.append(clean_line)
                 else:
                     d_strings[-1] += clean_line + " "
             if 'A|' in line:
-                if not a_strings or len(a_strings[-1]) + len(line) + 1 > 400:
+                if not a_strings or len(a_strings[-1]) + len(line) + 1 > max_length:
                     a_strings.append(clean_line)
                 else:
                     a_strings[-1] += clean_line + " "
             if 'E|' in line:
-                if not e_strings or len(e_strings[-1]) + len(line) + 1 > 400:
+                if not e_strings or len(e_strings[-1]) + len(line) + 1 > max_length:
                     e_strings.append(clean_line)
                 else:
                     e_strings[-1] += clean_line + " "
@@ -55,8 +56,6 @@ def extract_and_combine_tabs(text):
     combined_tabs = ""
     for g, d, a, e in zip(g_strings, d_strings, a_strings, e_strings):
         combined_tabs += 'G|' + g + '\n' + 'D|' + d + '\n' + 'A|' + a + '\n' + 'E|' + e + '\n' + '\n\n'
-
-#      
 
     return combined_tabs.strip()
 
@@ -66,10 +65,15 @@ def main():
     
     url = input("Enter the Ultimate Guitar URL for the bass tab: ")
     
+    if len(sys.argv) > 1:
+        max_length = int(sys.argv[1])
+    else:
+        max_length = 400
+    
     html_content = download_tab(url)
     
     if html_content:
-        combined_tabs = extract_and_combine_tabs(html_content)
+        combined_tabs = extract_and_combine_tabs(html_content, max_length)
         
         if combined_tabs:
             output_file = "combined_tab.txt"
